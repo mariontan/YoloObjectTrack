186f9040390a256e45437e1936e3c3db399a8942
AIC-WS1\IvanTan <marionltan@gmail.com>
2020-08-31 23:52:06 +0800

    confidence threshhold sweep from 0.9981 to 0.9985 interval 0.0001

--- loop_conf_Ivan_object_tracker.py
+++ loop_conf_Ivan_object_tracker.py
@@ -32,14 +32,36 @@ nms_thres=0.4
 
 carClass = 0
 
-confArr=[0.9988,0.9989]
+confArr=[0.9981,0.9982,0.9983,0.9984,0.9985]
+videoArr=[]
 
-# videoArr = [r'D:\Ivan\Test_data\Katipunan\test/VID_20200518_161529_crop.mp4']
 
 # videoArr = [r'D:/Ivan/Test_data/IvanMadeDataSet/Stanford_AI_cars_modified/car_crop.mp4']
 
-# videoArr=[r'D:\Ivan\Test_data\Katipunan\test/VID_20200518_161529_crop.mp4',r'D:\Ivan\Test_data\Katipunan\test/VID_20200521_161542_crop.mp4',r'D:\Ivan\Test_data\Katipunan\test/VID_20200530_161507_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200602_161933_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200605_161506_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200611_161534_crop.mp4',r'D:\Ivan\Test_data\Katipunan\test/VID_20200614_161517_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200620_161507_crop.mp4']
-videoArr=[r'D:\Ivan\Test_data\Katipunan\test/VID_20200521_161542_crop.mp4',r'D:\Ivan\Test_data\Katipunan\test/VID_20200530_161507_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200602_161933_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200605_161506_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200611_161534_crop.mp4',r'D:\Ivan\Test_data\Katipunan\test/VID_20200614_161517_crop.mp4',r'D:/Ivan/Test_data/Katipunan/test/VID_20200620_161507_crop.mp4']
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200509_161540_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200512_161525_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200515_161547_crop.mp4')
+# videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200518_161529_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200521_161542_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200530_161507_crop.mp4')
+videoArr.append(r'D:/Ivan/Test_data/Katipunan/test/VID_20200602_161933_crop.mp4')
+videoArr.append(r'D:/Ivan/Test_data/Katipunan/test/VID_20200605_161506_crop.mp4')
+videoArr.append(r'D:/Ivan/Test_data/Katipunan/test/VID_20200611_161534_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200614_161517_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200617_161508_crop.mp4')
+videoArr.append(r'D:/Ivan/Test_data/Katipunan/test/VID_20200620_161507_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200705_161518_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200708_161553_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200711_161516_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200714_161511_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200717_161614_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200720_161556_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200723_161518_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200726_161509_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200729_161519_crop.mp4')
+videoArr.append(r'D:\Ivan\Test_data\Katipunan\test/VID_20200801_161505_crop.mp4')
+
+print(len(videoArr))
 
 model = Darknet(config_path, img_size=img_size)
 
